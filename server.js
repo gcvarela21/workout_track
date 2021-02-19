6963d70f4cae1387b7e21aa3e58863107bb26512
gcvarela21 <gvarela2@horizon.csueastbay.edu>
2021-02-19 00:07:31 -0800

    added given files to this repo and started the server set up

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+// const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// app.use(require("./routes/routes.js"));
+
+app.listen(PORT, () => {
+  console.log("app is running biatch! It be on port ${PORT}!");
+});
